a20b1a609d8fee653814e6a7ec5e341b268e543a
sbrace <sbrace0822@gmail.com>
2022-10-26 13:14:05 +0800

    ch02 taco-cloud add validation

--- ch02/taco-cloud/src/main/java/tacos/Taco.java
+++ ch02/taco-cloud/src/main/java/tacos/Taco.java
@@ -2,10 +2,17 @@ package tacos;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @NotEmpty(message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 }
--- ch02/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ ch02/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,12 +3,15 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,9 +23,9 @@ import static tacos.Ingredient.Type;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    @GetMapping
-    public String showDesignForm(Model model) {
-        List<Ingredient> ingredientList = Arrays.asList(
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
                 new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
@@ -35,20 +38,28 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
-        Type[] types = Type.values();
+        Type[] types = Ingredient.Type.values();
         for (Type type : types) {
-            model.addAttribute(type.name().toLowerCase(), filterByType(ingredientList, type));
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
-        model.addAttribute("design", new Taco());
-        return "design";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
 
+    @GetMapping
+    public String showDesignForm(Model model) {
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design : {}", design);
         return "redirect:/orders/current";
     }
