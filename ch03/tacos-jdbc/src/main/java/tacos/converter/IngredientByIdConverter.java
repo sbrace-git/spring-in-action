7ac481e1c5d7fab58d694a7d7e9cf77e75b2d494
sbrace <sbrace0822@gmail.com>
2022-10-30 03:45:15 +0800

    ch03 tacos-jdbc add IngredientByIdConverterTest

--- ch03/tacos-jdbc/src/main/java/tacos/converter/IngredientByIdConverter.java
+++ ch03/tacos-jdbc/src/main/java/tacos/converter/IngredientByIdConverter.java
@@ -9,7 +9,7 @@ import tacos.repository.IngredientRepository;
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
-    private IngredientRepository ingredientRepo;
+    private final IngredientRepository ingredientRepo;
 
     @Autowired
     public IngredientByIdConverter(IngredientRepository ingredientRepo) {
