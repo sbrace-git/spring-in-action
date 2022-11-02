2d25d7f112dc9eb442578ee344b8a317c029325e
sbrace <sbrace0822@gmail.com>
2022-11-02 22:16:08 +0800

    ch04 taco-cloud update HomeControllerTest add @AutoConfigureMockMvc

--- ch04/taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ ch04/taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -2,31 +2,20 @@ package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import tacos.repository.IngredientRepository;
-import tacos.repository.OrderRepository;
-import tacos.repository.TacoRepository;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private IngredientRepository ingredientRepository;
-
-    @MockBean
-    private TacoRepository tacoRepository;
-
-    @MockBean
-    private OrderRepository orderRepository;
 
     @Test
     public void testHomePage() throws Exception {
