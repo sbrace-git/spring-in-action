06c5d614135eb21ae9967d7d2cf5adf4f9c68aa2
sbrace <sbrace0822@gmail.com>
2022-11-02 23:48:07 +0800

    ch04 taco-cloud update DesignTacoControllerTest add @WithMockUser

--- ch04/taco-cloud/src/test/java/tacos/DesignTacoControllerTest.java
+++ ch04/taco-cloud/src/test/java/tacos/DesignTacoControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.ui.ModelMap;
@@ -66,6 +67,7 @@ public class DesignTacoControllerTest {
 
 
     @Test
+    @WithMockUser
     public void testShowDesignForm() throws Exception {
         mockMvc.perform(get("/design"))
                 .andExpect(status().isOk())
