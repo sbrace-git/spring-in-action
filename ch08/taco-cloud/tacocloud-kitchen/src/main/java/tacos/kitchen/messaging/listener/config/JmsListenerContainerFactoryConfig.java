bc5edd77a3cae3da68bf480796acc514fe6e7dcb
sbrace <sbrace0822@gmail.com>
2022-11-12 22:50:16 +0800

    ch08 taco-cloud remove @EnableJms

--- ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/listener/config/JmsListenerContainerFactoryConfig.java
+++ ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/listener/config/JmsListenerContainerFactoryConfig.java
@@ -2,13 +2,11 @@ package tacos.kitchen.messaging.listener.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 
 import javax.jms.ConnectionFactory;
 
-@EnableJms
 @Configuration
 public class JmsListenerContainerFactoryConfig {
     @Bean
