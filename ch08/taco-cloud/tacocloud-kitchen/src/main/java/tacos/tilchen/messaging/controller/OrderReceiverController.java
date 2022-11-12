87f418865cdd8149c26983cd93823ae4bdec704c
sbrace <sbrace0822@gmail.com>
2022-11-12 20:50:57 +0800

    ch08 taco-cloud tacocloud-kitchen OrderReceiverController add log

--- ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/tilchen/messaging/controller/OrderReceiverController.java
+++ ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/tilchen/messaging/controller/OrderReceiverController.java
@@ -1,11 +1,13 @@
 package tacos.tilchen.messaging.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.tilchen.messaging.domain.Order;
 import tacos.tilchen.messaging.receiver.OrderReceiver;
 
+@Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderReceiverController {
@@ -19,8 +21,9 @@ public class OrderReceiverController {
     @RequestMapping("/receive")
     public String receiveOrder(Model model) {
         Order order = orderReceiver.receiveOrder();
+        log.info("receiveOrder order = {}", order);
         if (order != null) {
-            model.addAttribute("order",order);
+            model.addAttribute("order", order);
             return "receiveOrder";
         }
         return "noOrder";
