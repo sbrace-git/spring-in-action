26da1915a9a976e45f4bb64238d8dac98b8e5652
sbrace <sbrace0822@gmail.com>
2022-11-12 16:53:12 +0800

    ch08 taco-cloud Message getStringProperty

--- ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/tilchen/messaging/jms/JmsOrderReceiver.java
+++ ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/tilchen/messaging/jms/JmsOrderReceiver.java
@@ -1,21 +1,42 @@
 package tacos.tilchen.messaging.jms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
 import tacos.tilchen.messaging.domain.Order;
 import tacos.tilchen.messaging.receiver.OrderReceiver;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Slf4j
 @Component
 public class JmsOrderReceiver implements OrderReceiver {
 
     private JmsTemplate jmsTemplate;
 
-    public JmsOrderReceiver(JmsTemplate jmsTemplate) {
+    private MessageConverter messageConverter;
+
+    public JmsOrderReceiver(JmsTemplate jmsTemplate, MessageConverter messageConverter) {
         this.jmsTemplate = jmsTemplate;
+        this.messageConverter = messageConverter;
     }
+
+//    @Override
+//    public Order receiveOrder() {
+//        return (Order) jmsTemplate.receiveAndConvert();
+//    }
 
     @Override
     public Order receiveOrder() {
-        return (Order) jmsTemplate.receiveAndConvert();
+        Message receive = jmsTemplate.receive();
+        try {
+            String x_order_source = receive.getStringProperty("X_ORDER_SOURCE");
+            log.info("receiveOrder x_order_source = {}", x_order_source);
+            return (Order) messageConverter.fromMessage(receive);
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
