d6c11c699338d0004d47720d6a688dac59ac8595
sbrace <sbrace0822@gmail.com>
2022-11-14 23:02:26 +0800

    ch08 taco-cloud tacocloud-kitchen delete exchange

--- ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitMqMessagingConfig.java
+++ ch08/taco-cloud/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitMqMessagingConfig.java
@@ -1,7 +1,5 @@
 package tacos.kitchen.messaging.rabbit;
 
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -15,19 +13,7 @@ public class RabbitMqMessagingConfig {
     }
 
     @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange("tacocloud.order");
-    }
-
-    @Bean
     public Queue rabbitOrderQueue() {
         return new Queue("tacocloud.order");
-    }
-
-    @Bean
-    public Object bindingDirectExchangeOrderQueue() {
-        return BindingBuilder.bind(rabbitOrderQueue())
-                .to(directExchange())
-                .withQueueName();
     }
 }
