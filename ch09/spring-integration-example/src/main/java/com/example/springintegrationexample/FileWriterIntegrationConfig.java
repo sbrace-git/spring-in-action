3cf898f780bb5f87d349a692339999b7b4979ae0
sbrace <sbrace0822@gmail.com>
2022-11-16 22:28:46 +0800

    ch09 spring-integration xmlConfig

--- ch09/spring-integration-example/src/main/java/com/example/springintegrationexample/FileWriterIntegrationConfig.java
+++ ch09/spring-integration-example/src/main/java/com/example/springintegrationexample/FileWriterIntegrationConfig.java
@@ -2,10 +2,16 @@ package com.example.springintegrationexample;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@ImportResource("classpath:/filewriter-config.xml")
 public class FileWriterIntegrationConfig {
 
+    @Profile("xmlConfig")
+    @Configuration
+    @ImportResource("classpath:/filewriter-config.xml")
+    public static class XmlConfiguration {
+
+    }
 
 }
