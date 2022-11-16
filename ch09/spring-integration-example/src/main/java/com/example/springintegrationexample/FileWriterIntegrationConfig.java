9348820c352a33ca31c50e6e0fa47fc980c0f520
sbrace <sbrace0822@gmail.com>
2022-11-16 23:11:33 +0800

    ch09 spring-integration javaconfig

--- ch09/spring-integration-example/src/main/java/com/example/springintegrationexample/FileWriterIntegrationConfig.java
+++ ch09/spring-integration-example/src/main/java/com/example/springintegrationexample/FileWriterIntegrationConfig.java
@@ -1,8 +1,16 @@
 package com.example.springintegrationexample;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
+
+import java.io.File;
 
 @Configuration
 public class FileWriterIntegrationConfig {
@@ -14,4 +22,21 @@ public class FileWriterIntegrationConfig {
 
     }
 
+    @Profile("javaConfig")
+    @Bean
+    @Transformer(inputChannel = "textInchannel", outputChannel = "fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return String::toUpperCase;
+    }
+
+    @Profile("javaConfig")
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler fileWritingMessageHandler = new FileWritingMessageHandler(new File("D:\\common\\temp\\files"));
+        fileWritingMessageHandler.setExpectReply(false);
+        fileWritingMessageHandler.setFileExistsMode(FileExistsMode.APPEND);
+        fileWritingMessageHandler.setAppendNewLine(true);
+        return fileWritingMessageHandler;
+    }
 }
