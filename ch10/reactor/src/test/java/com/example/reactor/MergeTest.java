fc58a2badae7cde23da5a2ab55b5bca7c950bce3
sbrace <sbrace0822@gmail.com>
2022-11-24 11:43:14 +0800

    ch10 reactor test flux zip firstWithSignal

--- ch10/reactor/src/test/java/com/example/reactor/MergeTest.java
+++ ch10/reactor/src/test/java/com/example/reactor/MergeTest.java
@@ -3,13 +3,14 @@ package com.example.reactor;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 
 public class MergeTest {
 
     @Test
-    public void merge() {
+    public void print() {
         Flux.just("a", "b", "c", "d")
                 .delaySubscription(Duration.ofSeconds(2))
                 .delayElements(Duration.ofSeconds(2))
@@ -18,7 +19,7 @@ public class MergeTest {
     }
 
     @Test
-    public void mergeFluxes() {
+    public void flux_merge() {
         Flux<String> characterFlux = Flux
                 .just("Garfield", "Kojak", "Barbossa")
                 .delayElements(Duration.ofMillis(500));
@@ -34,6 +35,47 @@ public class MergeTest {
                 .expectNext("Lollipops")
                 .expectNext("Barbossa")
                 .expectNext("Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void flux_zip() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+        Flux<Tuple2<String, String>> zip = Flux.zip(characterFlux, foodFlux);
+        StepVerifier.create(zip)
+                .expectNextMatches(p -> p.getT1().equals("Garfield") && p.getT2().equals("Lasagna"))
+                .expectNextMatches(p -> p.getT1().equals("Kojak") && p.getT2().equals("Lollipops"))
+                .expectNextMatches(p -> p.getT1().equals("Barbossa") && p.getT2().equals("Apples"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void flux_zip_1() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+        Flux<String> zip = Flux.zip(characterFlux, foodFlux, (s1, s2) -> s1 + " eats " + s2);
+        StepVerifier.create(zip)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojak eats Lollipops")
+                .expectNext("Barbossa eats Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void firstFlux() {
+        Flux<String> slowFlux = Flux.just("tortoise", "snail", "sloth")
+                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+        Flux<String> firstFlux = Flux.firstWithSignal(slowFlux, fastFlux);
+        StepVerifier.create(firstFlux)
+                .expectNext("hare")
+                .expectNext("cheetah")
+                .expectNext("squirrel")
                 .verifyComplete();
     }
 }
