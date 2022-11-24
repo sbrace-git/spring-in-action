fd0d19a35f85af06ee73bbce4bc5aa0b2145ab72
sbrace <sbrace0822@gmail.com>
2022-11-24 19:24:56 +0800

    ch10 reactor test log

---
+++ ch10/reactor/src/test/java/com/example/reactor/FluxLoggingTests.java
@@ -0,0 +1,43 @@
+package com.example.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class FluxLoggingTests {
+
+  @Test
+  public void logSimple() {
+    Flux<String> beltColors = Flux.just(
+        "white", "yellow", "orange", "green", "purple", "blue")
+      .log();
+    beltColors.subscribe();
+  }
+  
+  @Test
+  public void logMapping() {
+    Flux<String> beltColors = Flux.just(
+        "white", "yellow", "orange", "green", "purple", "blue")
+      .map(String::toUpperCase)
+      .log()
+      ;
+    beltColors.subscribe(); 
+  }
+  
+  @Test
+  public void logFlatMapping() throws Exception {
+    Flux<String> beltColors = Flux.just(
+        "white", "yellow", "orange", "green", "purple", "blue")
+      .flatMap(cb -> Mono.just(cb)
+          .map(String::toUpperCase)
+          .log()
+          .subscribeOn(Schedulers.parallel())
+      )
+      ;
+    beltColors.subscribe(); 
+    
+    Thread.sleep(3000L);
+  }
+  
+}
