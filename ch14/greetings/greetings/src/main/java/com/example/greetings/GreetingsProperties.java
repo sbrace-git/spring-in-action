822ecacdd3de494cf29966f11bd35bd9bd36bbc1
sbrace <sbrace0822@gmail.com>
2022-12-10 18:49:26 +0800

    ch14 greetings add name

--- ch14/greetings/greetings/src/main/java/com/example/greetings/GreetingsProperties.java
+++ ch14/greetings/greetings/src/main/java/com/example/greetings/GreetingsProperties.java
@@ -9,6 +9,16 @@ public class GreetingsProperties {
 
     private String message;
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getMessage() {
         return message;
     }
--- ch14/greetings/greetings/src/main/java/com/example/greetings/HelloController.java
+++ ch14/greetings/greetings/src/main/java/com/example/greetings/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return greetingsProperties.getMessage();
+        return greetingsProperties.getMessage() + ", " + greetingsProperties.getName();
     }
 }
