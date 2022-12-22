9f21841745d319ff6a6215211a6b496575e643e3
sbrace <sbrace0822@gmail.com>
2022-12-22 10:54:37 +0800

    ch14 ingredient config-server add @EnableConfigServer

--- ch14/ingredient/config-server/src/main/java/com/tacos/configserver/ConfigServerApplication.java
+++ ch14/ingredient/config-server/src/main/java/com/tacos/configserver/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package com.tacos.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
