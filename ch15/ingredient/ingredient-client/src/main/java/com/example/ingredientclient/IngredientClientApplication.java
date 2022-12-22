ab9c2a2ccfe2a28d57b95c95aa01c4919168fb93
sbrace <sbrace0822@gmail.com>
2022-12-22 14:37:19 +0800

    ch15 ingredient-client EnableHystrix

--- ch15/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/IngredientClientApplication.java
+++ ch15/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/IngredientClientApplication.java
@@ -2,7 +2,9 @@ package com.example.ingredientclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
 @SpringBootApplication
 public class IngredientClientApplication {
 
--- ch15/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/resttemplate/IngredientServiceClient.java
+++ ch15/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/resttemplate/IngredientServiceClient.java
@@ -1,12 +1,15 @@
 package com.example.ingredientclient.resttemplate;
 
 import com.example.ingredientclient.Ingredient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 @Conditional(NotFeignAndNotWebClientCondition.class)
@@ -24,10 +27,22 @@ public class IngredientServiceClient {
                 Ingredient.class, ingredientId);
     }
 
+    @HystrixCommand(fallbackMethod = "getDefaultIngredients")
     public Iterable<Ingredient> getAllIngredients() {
+        int i = 1 / 0;
         Ingredient[] ingredients = rest.getForObject(
                 "http://ingredient-service/ingredients", Ingredient[].class);
         return Arrays.asList(ingredients);
     }
 
+    private Iterable<Ingredient> getDefaultIngredients() {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient(
+                "FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+        ingredients.add(new Ingredient(
+                "GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+        ingredients.add(new Ingredient(
+                "CHED", "Shredded Cheddar", Ingredient.Type.CHEESE));
+        return ingredients;
+    }
 }
