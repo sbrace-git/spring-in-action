2d422756305a3ce4a67e600b1f32ba440dcf85aa
sbrace <sbrace0822@gmail.com>
2023-01-02 23:53:22 +0800

    ch16 ingredient add info with InfoContributor

--- ch16/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/IngredientClientApplication.java
+++ ch16/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/IngredientClientApplication.java
@@ -3,10 +3,16 @@ package com.example.ingredientclient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableHystrix
 @SpringBootApplication
@@ -27,5 +33,16 @@ public class IngredientClientApplication {
             String phone = environment.getProperty("info.contact.phone");
             System.out.println("phone = " + phone);
         };
+    }
+
+    @Component
+    public static class MyInfo implements InfoContributor {
+        @Override
+        public void contribute(Info.Builder builder) {
+            Map<String, String> info = new HashMap<>();
+            info.put("name", "name");
+            info.put("key", "value");
+            builder.withDetail("author", info);
+        }
     }
 }
