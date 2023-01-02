c3a87a5347d6050458e51cc97960a412249ba809
sbrace <sbrace0822@gmail.com>
2023-01-02 23:47:10 +0800

    ch16 ingredient add info

--- ch16/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/IngredientClientApplication.java
+++ ch16/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/IngredientClientApplication.java
@@ -1,8 +1,12 @@
 package com.example.ingredientclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @EnableHystrix
 @SpringBootApplication
@@ -12,4 +16,16 @@ public class IngredientClientApplication {
         SpringApplication.run(IngredientClientApplication.class, args);
     }
 
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            String email = environment.getProperty("info.contact.email");
+            System.out.println("email = " + email);
+            String phone = environment.getProperty("info.contact.phone");
+            System.out.println("phone = " + phone);
+        };
+    }
 }
