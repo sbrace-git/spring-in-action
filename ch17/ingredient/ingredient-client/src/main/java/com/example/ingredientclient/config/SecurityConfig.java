c190fdac05174b0521572dba02e806aa8ec1c816
sbrace <sbrace0822@gmail.com>
2023-01-30 00:43:52 +0800

    ch17 admin service discovery

--- ch17/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/config/SecurityConfig.java
+++ ch17/ingredient/ingredient-client/src/main/java/com/example/ingredientclient/config/SecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
